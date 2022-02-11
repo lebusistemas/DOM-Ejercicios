4e7d0ae67f83c814449849e2027439416073f1f9
Leandro Espinoza <leandroespinoza@gmail.com>
2022-02-11 10:37:47 -0400

    Cuenta Regresiva

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,12 +1,14 @@
 import hamburgerMenu from "./dom/menu_hamburguesa.js";
 import { digitalClock, alarm } from "./dom/reloj.js";
 import { moveBall, shortcuts } from "./dom/teclado.js";
+import countdown from "./dom/cuenta_regresiva.js";
 
 const d = document;
 d.addEventListener("DOMContentLoaded", (e) => {
     hamburgerMenu(".panel-btn", ".panel", ".menu a");
     digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
     alarm("assets/alarma.mp3","#activar-alarma","#desactivar-alarma");
+    countdown("countdown", "Feb 11, 2022 10:37:19", "Llagamos a la meta");
 });
 
 d.addEventListener("keydown", (e) => {
