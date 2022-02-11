dea6a5fbdc5d5412cc80d10912ef0f27b0ad0a4a
Leandro Espinoza <leandroespinoza@gmail.com>
2022-02-11 17:15:44 -0400

    Detección de la cámara web

---
+++ js/dom/deteccion_webcam.js
@@ -0,0 +1,17 @@
+const d = document,
+      n = navigator;
+
+export default function webCam(id) {
+    const $video = d.getElementById(id);
+
+    if (n.mediaDevices.getUserMedia){
+        n.mediaDevices.getUserMedia({video: true, audio:false}).then((strem)=>{
+            console.log(strem);
+            $video.srcObject = strem;
+            $video.play();
+        }).catch((err)=> {
+            $video.insertAdjacentElement("afterend",`<p><mark>${err}</mark></p>`);
+            console.log(`¡Sucedio el siguiente error!: ${err}`);
+        });
+    }
+}
