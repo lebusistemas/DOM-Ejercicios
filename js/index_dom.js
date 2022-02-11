e2b0c5c724fc3e08dad7ee9c9c6f795013360fdd
Leandro Espinoza <leandroespinoza@gmail.com>
2022-02-11 09:11:15 -0400

    Eventos teclado 2

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,6 +1,6 @@
 import hamburgerMenu from "./dom/menu_hamburguesa.js";
 import { digitalClock, alarm } from "./dom/reloj.js";
-import { shortcuts } from "./dom/teclado.js";
+import { moveBall, shortcuts } from "./dom/teclado.js";
 
 const d = document;
 d.addEventListener("DOMContentLoaded", (e) => {
@@ -11,4 +11,5 @@ d.addEventListener("DOMContentLoaded", (e) => {
 
 d.addEventListener("keydown", (e) => {
     shortcuts(e);
+    moveBall(e, ".ball", ".stage");
 });
