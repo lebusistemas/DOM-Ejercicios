ce0b1b6d9186f23c44702231128d39b4a1ce728c
Leandro Espinoza <leandroespinoza@gmail.com>
2022-02-11 15:07:08 -0400

    Responsive Tester

--- js/index_dom.js
+++ js/index_dom.js
@@ -5,6 +5,7 @@ import countdown from "./dom/cuenta_regresiva.js";
 import scrollTopButton from "./dom/boton_scroll.js";
 import darkTheme from "./dom/tema_oscuro.js";
 import responsiveMedia from "./dom/objeto_responsive.js";
+import responsiveTester from "./dom/prueba_responsive.js";
 
 const d = document;
 d.addEventListener("DOMContentLoaded", (e) => {
@@ -22,6 +23,8 @@ d.addEventListener("DOMContentLoaded", (e) => {
         "(min-width: 1024px)",
         `<a href="https://goo.gl/maps/unAyYY9VQBiMbvhR7" target="_blank" rel="noopener">Ver Mapa</a>`,
         `<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d346.6178164089499!2d-71.6318368342897!3d10.615643200961046!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e8999da9f725a4f%3A0xb3a347d0b089a584!2sBarrio%20Altamira%20Sur%2C%20Maracaibo%204001%2C%20Zulia!5e0!3m2!1ses!2sve!4v1644604608945!5m2!1ses!2sve" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>`);
+
+    responsiveTester("responsive-tester");
 });
 
 d.addEventListener("keydown", (e) => {
