94afe3816265dd98dcca6079393d6ebe511385c3
Leandro Espinoza <leandroespinoza@gmail.com>
2022-02-11 12:18:58 -0400

    Tema Oscuro

--- js/index_dom.js
+++ js/index_dom.js
@@ -3,6 +3,7 @@ import { digitalClock, alarm } from "./dom/reloj.js";
 import { moveBall, shortcuts } from "./dom/teclado.js";
 import countdown from "./dom/cuenta_regresiva.js";
 import scrollTopButton from "./dom/boton_scroll.js";
+import darkTheme from "./dom/tema_oscuro.js";
 
 const d = document;
 d.addEventListener("DOMContentLoaded", (e) => {
@@ -17,3 +18,5 @@ d.addEventListener("keydown", (e) => {
     shortcuts(e);
     moveBall(e, ".ball", ".stage");
 });
+
+darkTheme(".dark-theme-btn", "dark-mode");
