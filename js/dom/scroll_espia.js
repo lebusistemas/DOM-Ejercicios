bf44683ba4f09a2d54c6cb76c5f3319f4220d2bb
Leandro Espinoza <leandroespinoza@gmail.com>
2022-02-12 15:07:43 -0400

    Intersection Observer

---
+++ js/dom/scroll_espia.js
@@ -0,0 +1,28 @@
+const d = document;
+
+export default function scrollSpy() {
+    const $sections = d.querySelectorAll("section[data-scroll-spy]");
+    const cb = (entries) => {
+        //console.log("Entries", entries);
+        
+        entries.forEach((entry)=>{
+            const id = entry.target.getAttribute("id");
+
+            //console.log("Entry", entry);
+            if (entry.isIntersecting){
+                d.querySelector(`a[data-scroll-spy][href="#${id}"]`).classList.add("active");
+            } else {
+                d.querySelector(`a[data-scroll-spy][href="#${id}"]`).classList.remove("active");
+            }
+        });
+    };
+
+    const observer = new IntersectionObserver(cb, {
+        //root
+        //rootMargin: "-250px"
+        threshold: [0.5, 0.75]
+    });
+    //console.log("observer", observer);
+
+    $sections.forEach(el => observer.observe(el));
+}
--- js/index_dom.js
+++ js/index_dom.js
@@ -13,6 +13,7 @@ import getGeolocation from "./dom/geolocalizacion.js";
 import searchFilters from "./dom/filtro_busquedas.js";
 import draw from "./dom/Sorteo.js";
 import slider from "./dom/carrusel.js";
+import scrollSpy from "./dom/scroll_espia.js";
 
 const d = document;
 d.addEventListener("DOMContentLoaded", (e) => {
@@ -38,6 +39,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
     searchFilters(".card-filter", ".card");
     draw("#winner-btn", ".player");
     slider();
+    scrollSpy();
 });
 
 d.addEventListener("keydown", (e) => {
